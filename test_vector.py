2f52bffe9d9a0a8328f3822681dccf780b28a917
Che1989 <che3108@gmail.com>
2022-10-14 14:26:50 +0800

    modified:   test_vector.py

--- test_vector.py
+++ test_vector.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 from vectors import Vector_2D
+import time
 
-v = Vector_2D((2,2), 3, 30)
+t_start = time.time()
+v = Vector_2D((2,2), 3, 1000000000000)
+print(time.time() - t_start)
